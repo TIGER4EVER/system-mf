47c71d5f0707144ea187eb04174e655e1248042f
TIGER4EVER <47013568+TIGER4EVER@users.noreply.github.com>
2019-01-30 15:05:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "NDIyMDM3NzQwNTkxMzgyNTI4.DYV8zA.6vBJjF7Op9T_bH_YOwRMXqwh9vc";
+const discord_token = "NTQwMTQxMjQwMDIxNDE3OTk0.DzMlaA.d1slZwrUXMjZcOJx2opvb1QtaQs";
 var table = require('table').table
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -17,7 +17,7 @@ client.on('ready', () => {
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,' Premium Bot™ by n3k4a & Baron','Type -help |-support |-invite ',`${client.users.size} Members`,'-invite','By: n3k4a & Hosam | BaronTube'];
+    var setGame = [`${client.guilds.size} Server`,' Premium Bot™ by tiger & jamika','Type -help |-support |-invite ',`${client.users.size} Members`,'-invite','By: tiger & jamika | BaronTube'];
     var i = -1;
     var j = 0;
     setInterval(function (){
